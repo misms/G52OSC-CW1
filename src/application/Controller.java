c1a8bf05d45356cae932e8eba16d2672460a3e18
Ken Poon Kang Lun <khcy5pkl@nottingham.edu.my>
2016-11-24 23:32:03 +0800

    Handle Exception with Alert Box; GUI results

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -3,25 +3,23 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-
 import javafx.scene.control.TextField;
 import java.util.StringTokenizer;
+import javax.swing.text.DocumentFilter;
 
 
 public class Controller extends Algorithms implements Initializable {
 
-	ObservableList<String> algorithm_list = FXCollections.observableArrayList("FCFS","SSTF","SCAN","CSCAN","LOOK","CLOOK");
+    //Combo Box -> Algorithms
+	private ObservableList<String> algorithm_list = FXCollections.observableArrayList("FCFS","SSTF","SCAN","CSCAN","LOOK","CLOOK");
 
-    public String requestString;
-	@FXML
-	private Button simulate_button = new Button();
+
     @FXML
     TextField current_pos = new TextField();
     @FXML
@@ -33,39 +31,45 @@ public class Controller extends Algorithms implements Initializable {
 	@FXML
 	private ComboBox algobox = new ComboBox();
 
-
-    private int cylinderSIZE;
-    private int currentPOS;
-    private int numREQUEST;
-    private int[] requestARRAY;
-    private int selALGORITHM;
+	/**
+     * Set requestARRAY size
+     * Get String from request textfield
+     * Set token "," and parse Integer into requestARRAY for initialization
+     * Call Disk constructor and intialize all variables
+     * Select algorithms
+     * @throws IOException
+     */
 
 	@FXML
 	public void simulate() throws IOException {
         int x = 0;
-        int[] requestARRAY = new int[Integer.parseInt(num_request.getText())];
-        requestString = request.getText().toString();
-        StringTokenizer str = new StringTokenizer(requestString,",");
-        while(str.hasMoreTokens()) {
-            requestARRAY[x] = Integer.parseInt(str.nextToken());
-            x++;
-        }
 
-        Disk disk1 = new Disk(Integer.parseInt(cylinder_size.getText()),
-                Integer.parseInt(current_pos.getText()),
-                Integer.parseInt(num_request.getText()),
-                requestARRAY);
-	    select_algorithms(disk1);
+		try {
+			int[] requestARRAY = new int[Integer.parseInt(num_request.getText())];
+			StringTokenizer str = new StringTokenizer(request.getText(), ",");
+			while (str.hasMoreTokens()) {
+				requestARRAY[x] = Integer.parseInt(str.nextToken());
+				x++;
+			}
+			Disk disk1 = new Disk(Integer.parseInt(cylinder_size.getText()),
+					Integer.parseInt(current_pos.getText()),
+					Integer.parseInt(num_request.getText()),
+					requestARRAY);
+			select_algorithms(disk1);
+		}catch(NumberFormatException e){
+			Alert.display("Alert","Please use integer and comma specified only!");
+		}
 	}
 
-	public void select_algorithms(Disk disk){
+
+	private void select_algorithms(Disk disk){
 		int sel = 0;
 
         if(algobox.getValue() == "FCFS")
 		    fcfs(disk);
 
 		if(algobox.getValue() == "SSTF")
-			sel = 2;
+			sstf(disk);
 
 		if(algobox.getValue() == "SCAN")
 			sel = 3;
@@ -83,6 +87,7 @@ public class Controller extends Algorithms implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		algobox.setItems(algorithm_list);
+		//Initialize combo box with algorithms
+        algobox.setItems(algorithm_list);
 	}
 }
--- src/application/Disk.java
+++ src/application/Disk.java
@@ -7,6 +7,7 @@ import java.lang.reflect.InvocationTargetException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
@@ -14,33 +15,44 @@ import javax.security.auth.login.Configuration;
 
 public class Disk extends Application {
 
-    static int cylinder;
-    static int startPos;
-    static int numberOfRequest;
-    static int[] request;
+    int cylinder;
+    int startPos;
+    int numberOfRequest;
+    int[] request;
 
+    @FXML
+    Button simulate_button = new Button();
 
     public Disk(){
-
     }
 
+    /**
+     * Initialize all variables
+     * @param size Cylinder size
+     * @param position Current header position
+     * @param numOfReq Number of requests
+     * @param request_array All requests
+     */
     public Disk(int size, int position,int numOfReq,int[] request_array) {
         cylinder = size;
         startPos = position;
         numberOfRequest = numOfReq;
         request = new int[numOfReq];
         request = request_array;
-
     }
 
+    //Set scene size and title
     @Override
     public void start(Stage primaryStage) throws Exception {
             Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
-            Scene scene = new Scene(root, 450, 550);
+            Scene scene = new Scene(root, 470, 700);
+            scene.getStylesheets().add("/application/application.css");
             primaryStage.setTitle("Disk Scheduling");
             primaryStage.setScene(scene);
             primaryStage.show();
     }
+
+
 	
 	public static void main(String[] args)
     {
